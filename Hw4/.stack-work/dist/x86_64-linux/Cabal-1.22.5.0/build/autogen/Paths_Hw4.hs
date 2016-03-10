28dc9eab644bcf77f668ec3169f41ed144ace38a
kunhuang <kunhuang110@126.com>
2016-03-10 14:53:41 -0800

    .gitignore

--- Hw4/.stack-work/dist/x86_64-linux/Cabal-1.22.5.0/build/autogen/Paths_Hw4.hs
+++ Hw4/.stack-work/dist/x86_64-linux/Cabal-1.22.5.0/build/autogen/Paths_Hw4.hs
@@ -16,11 +16,11 @@ version :: Version
 version = Version [1,0] []
 bindir, libdir, datadir, libexecdir, sysconfdir :: FilePath
 
-bindir     = "/home/cse230/Desktop/Hw4/.stack-work/install/x86_64-linux/nightly-2016-01-06/7.10.3/bin"
-libdir     = "/home/cse230/Desktop/Hw4/.stack-work/install/x86_64-linux/nightly-2016-01-06/7.10.3/lib/x86_64-linux-ghc-7.10.3/Hw4-1.0-Lu6RtrOiWHuLZpns6goq7j"
-datadir    = "/home/cse230/Desktop/Hw4/.stack-work/install/x86_64-linux/nightly-2016-01-06/7.10.3/share/x86_64-linux-ghc-7.10.3/Hw4-1.0"
-libexecdir = "/home/cse230/Desktop/Hw4/.stack-work/install/x86_64-linux/nightly-2016-01-06/7.10.3/libexec"
-sysconfdir = "/home/cse230/Desktop/Hw4/.stack-work/install/x86_64-linux/nightly-2016-01-06/7.10.3/etc"
+bindir     = "/home/cse230/Desktop/COURSE-CSE-230-Programming-Lanuage/Hw4/.stack-work/install/x86_64-linux/nightly-2016-01-06/7.10.3/bin"
+libdir     = "/home/cse230/Desktop/COURSE-CSE-230-Programming-Lanuage/Hw4/.stack-work/install/x86_64-linux/nightly-2016-01-06/7.10.3/lib/x86_64-linux-ghc-7.10.3/Hw4-1.0-Lu6RtrOiWHuLZpns6goq7j"
+datadir    = "/home/cse230/Desktop/COURSE-CSE-230-Programming-Lanuage/Hw4/.stack-work/install/x86_64-linux/nightly-2016-01-06/7.10.3/share/x86_64-linux-ghc-7.10.3/Hw4-1.0"
+libexecdir = "/home/cse230/Desktop/COURSE-CSE-230-Programming-Lanuage/Hw4/.stack-work/install/x86_64-linux/nightly-2016-01-06/7.10.3/libexec"
+sysconfdir = "/home/cse230/Desktop/COURSE-CSE-230-Programming-Lanuage/Hw4/.stack-work/install/x86_64-linux/nightly-2016-01-06/7.10.3/etc"
 
 getBinDir, getLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
 getBinDir = catchIO (getEnv "Hw4_bindir") (\_ -> return bindir)
